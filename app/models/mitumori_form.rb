19686e909450edc08068ce3bdfe999567880f1ec
s-yamane-tech <s-yamane@tech-mart.co.jp>
2021-06-29 14:36:41 +0900

    メール用モデル仮カラム作成及びバリデーション処理追加

--- app/models/mitumori_form.rb
+++ app/models/mitumori_form.rb
@@ -1,4 +1,7 @@
 class MitumoriForm
     include ActiveModel::Model
   
-  end
+    attr_accessor :mitumori_name, :name, :email, :radio, :detail
+    
+    validates :mitumori_name, :name, :email, :detail, presence: true
+  end 
