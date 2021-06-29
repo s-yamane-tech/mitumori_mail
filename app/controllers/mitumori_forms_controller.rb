9cdfbdf6b273d97cc229d50e6c8fa8fbf7123c7b
s-yamane-tech <s-yamane@tech-mart.co.jp>
2021-06-29 14:35:40 +0900

    確認画面遷移時コントローラ設定

--- app/controllers/mitumori_forms_controller.rb
+++ app/controllers/mitumori_forms_controller.rb
@@ -1,6 +1,25 @@
 class MitumoriFormsController < ApplicationController
+    # ログイン後のみアクセス可能
     before_action :authenticate_user!
+
+    # メールフォーム入力画面遷移時
     def new
         @mitumori_form = MitumoriForm.new
     end
+
+		 # 確認画面遷移時
+    def confirm
+        @mitumori_form = MitumoriForm.new(mitumori_form_params)
+        if @mitumori_form.valid?
+          render :confirm
+        else
+          render :new
+        end
+    end
+
+		# 入力したデータをパラメータとしてまとめる
+  	private
+  	def mitumori_form_params
+    	params.require(:mitumori_form).permit(:mitumori_name, :name, :email, :radio, :detail)
+  	end
 end
