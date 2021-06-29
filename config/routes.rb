7afa5f9e96ca570394bfd8349935f7cd7cd63abb
s-yamane-tech <s-yamane@tech-mart.co.jp>
2021-06-29 13:58:45 +0900

    ログイン後の遷移先設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+　
+  # ログイン用ルーティング
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # ログイン後の遷移先ルーティング
+  root 'mitumori_forms#new'
 end
