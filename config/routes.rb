00f5c9586e0dd5d0499c2655e5cc4143d76b2280
s-yamane-tech <s-yamane@tech-mart.co.jp>
2021-06-29 14:34:27 +0900

    確認画面遷移先ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-　
   # ログイン用ルーティング
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # ログイン後の遷移先ルーティング
   root 'mitumori_forms#new'
+
+  # 確認画面遷移先ルーティング
+  post 'mitumori_forms/confirm'
 end
